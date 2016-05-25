6d44b020158f0a1820b6d4349a5c02b67e99dcfa
Arjay Adong <arjayads@gmail.com>
2016-05-25 21:10:46 +0800

    update tracking

--- src/Statsd/Statsd.php
+++ src/Statsd/Statsd.php
@@ -8,16 +8,22 @@ use Domnikl\Statsd\Connection\UdpSocket;
 class Statsd {
 
     private static $client;
+    private static $tracking = true;
 
     public static function init() {
-        if (env('STATSD_TRACKING', false) === true) {
-            $connection = new UdpSocket(env('STATSD_HOST', 'localhost'), env('STATSD_PORT', 8125));
-            self::$client = new Client($connection, env('STATSD_NAMESPACE', ''));
-        }
+        $connection = new UdpSocket(env('STATSD_HOST', 'localhost'), env('STATSD_PORT', 8125));
+        self::$client = new Client($connection, env('STATSD_NAMESPACE', ''));
+        self::$tracking = env('STATSD_TRACKING', false) === true;
     }
 
     public static function client(){
         if (is_null(self::$client)) self::init();
         return self::$client;
+    }
+
+    public static function increment($key){
+        if (self::$tracking) {
+            self::client()->increment($key);
+        }
     }
 }
